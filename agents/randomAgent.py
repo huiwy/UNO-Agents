631ef6fb9c72da40c06f465ec6013c20385a9232
Melia Liu <liumy1@shanghaitech.edu.cn>
2020-12-12 20:33:40 +0800

    Added a class member variable 'name'.

--- agents/randomAgent.py
+++ agents/randomAgent.py
@@ -4,8 +4,9 @@ from .constants import DECK, INT2CARD, CARD2INT, COLORS
 from numpy.random import choice
 
 class RandomAgent(BaseAgent):
-  def __init__(self):
+  def __init__(self, name='RandomAgent'):
     super().__init__()
+    self.name = name
 
   def get_action(self, state, possible_actions):
     return choice(possible_actions), choice(4)
