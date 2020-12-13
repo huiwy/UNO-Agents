ebf907f18780e65001defb8895217ad767d0986e
Melia Liu <liumy1@shanghaitech.edu.cn>
2020-12-13 12:28:56 +0800

    Test ExpectimaxAgent.

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-# combine all thing together
+# combine all the things together
+from game import play
+from agents.humanAgent import HumanAgent
+from agents.randomAgent import RandomAgent
+from agents.expectimaxAgent import ExpectimaxAgent
+
+import numpy as np
+
+agent0 = ExpectimaxAgent()
+agent1 = RandomAgent()
+agent2 = RandomAgent()
+agent3 = RandomAgent()
+
+
+
+
+repeat = 1000
+agents = [agent0, agent1, agent2, agent3]
+winner_record = np.zeros(repeat)
+turns_record = np.zeros(repeat)
+
+for i in range(repeat):
+    winner, turns = play(agents)
+
+    winner_record[i] = winner
+    turns_record[i] = turns
+
+winning_rate = len(winner_record[winner_record==0])/len(winner_record)
+avg_turns = np.average(turns_record)
+
+print('='*10 + '%s' % repeat + ' games in total' + '='*10 )
+print('Participants:')
+for i in range(len(agents)):
+    print('\t' + 'agent%s: %s' % (i, agents[i].name))
+
+print('-'*10)
+print('winning rate of agent0: %s' % winning_rate)
+print('average turns: %s' % avg_turns)
+
